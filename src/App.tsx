7a107ad0738a19fc31978eb9d2a20fec72c8703b
Gabriel Gomes <gabrielgomes98@gmail.com>
2025-06-09 18:58:10 -0300

    fix: use nunito font

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { About, NavBar, Presentation, Projects } from "./components";
+import "./style.css";
 
 export function App() {
   return (
-    <div className="min-w-dvw bg-gradient-to-t from-black to-zinc-950 ">
+    <div className=" bg-gradient-to-t from-black to-zinc-950 overflow-x-hidden">
       <div className="box-border min-h-dvh grid grid-cols-1 place-items-center ">
         <NavBar />
         <Presentation />
