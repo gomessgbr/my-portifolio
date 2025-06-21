eb3007662631654494774d49d07ed1d7d7057e27
Gabriel Gomes <gabrielgomes98@gmail.com>
2025-06-21 15:38:33 -0300

    refactor: useless deploy

--- src/services/getRepos/getVercelRepos.ts
+++ src/services/getRepos/getVercelRepos.ts
@@ -1,52 +1,52 @@
-import { api } from "../api/config";
+// import { api } from "../api/config";
 
-interface IVercelReposResponse {
-  uid: string; //"dpl_5u4PnDhq2JawPYJbiDMicsZ2cgZX",
-  name: string; //"mp-lista-de-paises-next";
-  url: string; //"mp-lista-de-paises-next-2lrcee7br-gabriels-projects-8303da80.vercel.app";
-  created: string; //1750524663911;
-  source: string; //"git";
-  state: string; // "READY";
-  readyState: string; //"READY";
-  readySubstate: string; //"PROMOTED";
-  type: string; //"LAMBDAS";
-  creator: {
-    uid: string; //"PwISSZXZyyiHyJaZ2dG0c18B";
-    email: string; // "gabrielgomes98@gmail.com";
-    username: string; // "gomessgbr";
-    githubLogin: string; // "gomessgbr";
-  };
-  inspectorUrl: string; //"https://vercel.com/gabriels-projects-8303da80/mp-lista-de-paises-next/5u4PnDhq2JawPYJbiDMicsZ2cgZX";
-  meta: {
-    githubCommitAuthorName: string; //"Gabriel Gomes";
-    githubCommitMessage: string; //"refactor: new fields and delete olds fields";
-    githubCommitOrg: string; //"gomessgbr";
-    githubCommitRef: string; //"main";
-    githubCommitRepo: string; //"mp-lista-de-paises-next";
-    githubCommitSha: string; //"8d325d5a5751849ae2c0bceeeaf312a4f3ed21cd";
-    githubDeployment: string; //"1";
-    githubOrg: string; //"gomessgbr";
-    githubRepo: string; //"mp-lista-de-paises-next";
-    githubRepoOwnerType: string; //"User";
-    githubCommitRepoId: "string; //901834169";
-    githubRepoId: string; //"901834169";
-    githubRepoVisibility: string; //"public";
-    githubHost: string; //"github.com";
-    githubCommitAuthorLogin: string; //"gomessgbr";
-    branchAlias: string; //"mp-lista-de-paises-next-git-main-gabriels-projects-8303da80.vercel.app";
-  };
-  target: string; //"production";
-  aliasError: null;
-  aliasAssigned: number; //1750524699490;
-  isRollbackCandidate: boolean; //false;
-  createdAt: number; //1750524663911;
-  buildingAt: number; //1750524665023;
-  ready: number; //1750524699294;
-  projectSettings: {
-    commandForIgnoringBuildStep: null;
-  };
-}
-export const getVercelRepos = async () => {
-  const response = await api.get("deployments");
-  return response;
-};
+// interface IVercelReposResponse {
+//   uid: string; //"dpl_5u4PnDhq2JawPYJbiDMicsZ2cgZX",
+//   name: string; //"mp-lista-de-paises-next";
+//   url: string; //"mp-lista-de-paises-next-2lrcee7br-gabriels-projects-8303da80.vercel.app";
+//   created: string; //1750524663911;
+//   source: string; //"git";
+//   state: string; // "READY";
+//   readyState: string; //"READY";
+//   readySubstate: string; //"PROMOTED";
+//   type: string; //"LAMBDAS";
+//   creator: {
+//     uid: string; //"PwISSZXZyyiHyJaZ2dG0c18B";
+//     email: string; // "gabrielgomes98@gmail.com";
+//     username: string; // "gomessgbr";
+//     githubLogin: string; // "gomessgbr";
+//   };
+//   inspectorUrl: string; //"https://vercel.com/gabriels-projects-8303da80/mp-lista-de-paises-next/5u4PnDhq2JawPYJbiDMicsZ2cgZX";
+//   meta: {
+//     githubCommitAuthorName: string; //"Gabriel Gomes";
+//     githubCommitMessage: string; //"refactor: new fields and delete olds fields";
+//     githubCommitOrg: string; //"gomessgbr";
+//     githubCommitRef: string; //"main";
+//     githubCommitRepo: string; //"mp-lista-de-paises-next";
+//     githubCommitSha: string; //"8d325d5a5751849ae2c0bceeeaf312a4f3ed21cd";
+//     githubDeployment: string; //"1";
+//     githubOrg: string; //"gomessgbr";
+//     githubRepo: string; //"mp-lista-de-paises-next";
+//     githubRepoOwnerType: string; //"User";
+//     githubCommitRepoId: "string; //901834169";
+//     githubRepoId: string; //"901834169";
+//     githubRepoVisibility: string; //"public";
+//     githubHost: string; //"github.com";
+//     githubCommitAuthorLogin: string; //"gomessgbr";
+//     branchAlias: string; //"mp-lista-de-paises-next-git-main-gabriels-projects-8303da80.vercel.app";
+//   };
+//   target: string; //"production";
+//   aliasError: null;
+//   aliasAssigned: number; //1750524699490;
+//   isRollbackCandidate: boolean; //false;
+//   createdAt: number; //1750524663911;
+//   buildingAt: number; //1750524665023;
+//   ready: number; //1750524699294;
+//   projectSettings: {
+//     commandForIgnoringBuildStep: null;
+//   };
+// }
+// export const getVercelRepos = async () => {
+//   const response = await api.get("deployments");
+//   return response;
+// };
