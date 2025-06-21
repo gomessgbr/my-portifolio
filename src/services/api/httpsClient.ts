da048a2df159f458e2502b1d74c5e0333394c57e
Gabriel Gomes <gabrielgomes98@gmail.com>
2025-06-21 15:35:46 -0300

    wip: http client generic

---
+++ src/services/api/httpsClient.ts
@@ -0,0 +1,12 @@
+// interface ISendRequestProps<T, S, E> {
+//   requestFn: () => Promise<T>;
+//   onSuccess?: (data: S) => void;
+//   onError?: (error: E) => void;
+// }
+
+// interface ISendRequestPropsResponse<T, S, E> {
+//   isLoading: boolean;
+//   data: T;
+// }
+
+// export const sendRequest = (props: ISendRquestProps) => {};
